5c4828f8c105d9ebf12781971f7dd8cba81c598a
jun-130 <happy.smile.p0p0@gmail.com>
2022-08-24 11:09:42 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    # 変数postの値を、postというキーとセットでJavaScriptに送信
+    render json:{ post: post }
   end
 end
